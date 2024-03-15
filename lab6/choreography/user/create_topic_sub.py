156d32033d5b2e01971cffff4d465da77ea65c92
GeorgeAntono <44176403+GeorgeAntono@users.noreply.github.com>
2024-03-15 11:05:53 +0100

    Update create_topic_sub.py

--- lab6/choreography/user/create_topic_sub.py
+++ lab6/choreography/user/create_topic_sub.py
@@ -4,14 +4,14 @@ from user_consumer import create_subscription
 from user_publisher import create_topic
 
 logging.getLogger().setLevel(logging.INFO)
-create_topic("your_project_id", "order_req") # make sure to change the project id
-create_subscription("your_project_id", "order_req", "order_req_sub")
-create_topic("your_project_id", "inventory_status")
-create_topic("your_project_id", "order_status")
-create_topic("your_project_id", "order_status_user")
-create_subscription("your_project_id", "order_status_user", "order_status_user_sub")
-create_topic("your_project_id", "order_req")
-create_topic("your_project_id", "inventory_status")
-create_subscription("your_project_id", "inventory_status",
+create_topic("adapersonal", "order_req") # make sure to change the project id
+create_subscription("adapersonal", "order_req", "order_req_sub")
+create_topic("adapersonal", "inventory_status")
+create_topic("adapersonal", "order_status")
+create_topic("adapersonal", "order_status_user")
+create_subscription("adapersonal", "order_status_user", "order_status_user_sub")
+create_topic("adapersonal", "order_req")
+create_topic("adapersonal", "inventory_status")
+create_subscription("adapersonal", "inventory_status",
                     "inventory_status_sub")
-create_subscription("your_project_id", "order_status","order_status_sub")
+create_subscription("adapersonal", "order_status","order_status_sub")
